f7bb66809cef37b59ff87814d8b73f38c4ce81f2
d-perado <sjm01065@naver.com>
2026-01-01 17:24:32 +0900

    feat: 로그인 기능 구현

---
+++ src/main/java/com/example/jeogieottae/domain/auth/dto/request/SignInRequest.java
@@ -0,0 +1,17 @@
+package com.example.jeogieottae.domain.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SignInRequest {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}
