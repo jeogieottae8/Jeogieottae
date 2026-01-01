2a2584e028bef580863869eb3b10d77cd567019d
d-perado <sjm01065@naver.com>
2026-01-01 15:24:14 +0900

    feat: jwt 구현 및 회원가입 구현

--- src/main/java/com/example/jeogieottae/domain/auth/service/AuthService.java
+++ src/main/java/com/example/jeogieottae/domain/auth/service/AuthService.java
@@ -1,4 +1,34 @@
 package com.example.jeogieottae.domain.auth.service;
 
+import com.example.jeogieottae.common.util.JwtUtil;
+import com.example.jeogieottae.common.util.PasswordEncoder;
+import com.example.jeogieottae.domain.auth.dto.request.SignUpRequest;
+import com.example.jeogieottae.domain.auth.dto.response.SignUpResponse;
+import com.example.jeogieottae.domain.user.entity.User;
+import com.example.jeogieottae.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    @Transactional
+    public SignUpResponse signUp(SignUpRequest request) {
+        String userEmail = request.getEmail();
+        String username = request.getUsername();
+
+        User user = User.from(userEmail, username, passwordEncoder.encode(request.getPassword()));
+
+        userRepository.save(user);
+
+        String token = jwtUtil.generateToken(username, userEmail, user.getId());
+
+        return new SignUpResponse(token);
+    }
 }
