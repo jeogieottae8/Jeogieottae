75613dc04acc6ca99ca0dacc99129eae604f7a37
윤지현 <139779783+jheony@users.noreply.github.com>
2026-01-08 02:22:48 +0900

    refactor: ForwardedHeaderFilter URL 패턴 제한 적용
    - /accommodations/** 요청에만 필터 적용
    - 특정 경로에만 IP 기반 처리 적용

--- src/main/java/com/example/jeogieottae/common/config/WebMvcConfig.java
+++ src/main/java/com/example/jeogieottae/common/config/WebMvcConfig.java
@@ -1,11 +1,15 @@
 package com.example.jeogieottae.common.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 public class WebMvcConfig {
     @Bean
-    public ForwardedHeaderFilter forwardedHeaderFilter() {
-        return new ForwardedHeaderFilter();
+    FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new ForwardedHeaderFilter());
+        bean.addUrlPatterns("/accommodations/**");
+        return bean;
     }
 }
